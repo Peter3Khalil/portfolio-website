680b115bfb57b8bf540789d7d45d9a3fc19e1817
Peter Khalil <rikokamil97@gmail.com>
2024-02-20 02:51:46 +0200

    Adding google verification ownership

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,14 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="google-site-verification"
+          content="xOscsQA2aaavyLUSqlux4zQSo7tmpI7oIReqwnqy0FI"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
