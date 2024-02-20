747a5dafc297a76c00a7c140438f9b24267d1906
Peter Khalil <rikokamil97@gmail.com>
2024-02-20 15:30:34 +0200

    Remove lazy loading

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,13 +5,10 @@ import { links } from '../../public/data';
 import useObserver from '../hooks/useObserver';
 import { cn } from '../utils/helperFunctions';
 import Head from 'next/head';
-
-//Implement Lazy Loading for sections
-
-const About = React.lazy(() => import('../components/sections/About'));
-const Contact = React.lazy(() => import('../components/sections/Contact'));
-const Hero = React.lazy(() => import('../components/sections/Hero'));
-const Projects = React.lazy(() => import('../components/sections/Projects'));
+import Hero from '../components/sections/Hero';
+import Projects from '../components/sections/Projects';
+import About from '../components/sections/About';
+import Contact from '../components/sections/Contact';
 
 const Home = () => {
   const { visibleElement } = useObserver({ parentId: 'container' });
@@ -37,49 +34,46 @@ const Home = () => {
           property="og:description"
           content="Peter Khalil is a frontend developer who specializes in building high-quality, responsive, and accessible websites and web applications."
         />
-        <link rel="icon"  href="/profile.jpg" />
+        <link rel="icon" href="/profile.jpg" />
       </Head>
       <main className="flex h-[100svh] w-full flex-col bg-background text-foreground transition-all duration-500 ease-in-out">
         <Header activeLink={visibleElement?.id} />
         <div className="mb-14 flex h-auto w-full flex-col overflow-auto md:mb-0">
           <div id="container" className="h-auto">
-            <React.Suspense fallback={<div>Loading...</div>}>
-              <Hero />
-              <Projects />
-              <About />
-              <Contact />
-            </React.Suspense>
+            <Hero />
+            <Projects />
+            <About />
+            <Contact />
           </div>
           <Footer />
         </div>
         <nav>
-
-        <ul
-          role="navigation"
-          className="fixed bottom-0 left-0 flex h-14 w-full items-center justify-between border-t bg-primary px-4 dark:border-foreground/30 md:hidden"
-        >
-          {links.map((link) => {
-            return (
-              <li
-                onClick={() => {
-                  document
-                    .getElementById(link.id)
-                    .scrollIntoView({ behavior: 'smooth' });
-                }}
-                className={cn(
-                  'flex h-full cursor-pointer items-center justify-center border-t-4 border-transparent text-muted',
-                  {
-                    'border-background font-bold text-primary-foreground':
-                      link.id === visibleElement?.id,
-                  },
-                )}
-                key={link.id}
-              >
-                {link.name}
-              </li>
-            );
-          })}
-        </ul>
+          <ul
+            role="navigation"
+            className="fixed bottom-0 left-0 flex h-14 w-full items-center justify-between border-t bg-primary px-4 dark:border-foreground/30 md:hidden"
+          >
+            {links.map((link) => {
+              return (
+                <li
+                  onClick={() => {
+                    document
+                      .getElementById(link.id)
+                      .scrollIntoView({ behavior: 'smooth' });
+                  }}
+                  className={cn(
+                    'flex h-full cursor-pointer items-center justify-center border-t-4 border-transparent text-muted',
+                    {
+                      'border-background font-bold text-primary-foreground':
+                        link.id === visibleElement?.id,
+                    },
+                  )}
+                  key={link.id}
+                >
+                  {link.name}
+                </li>
+              );
+            })}
+          </ul>
         </nav>
       </main>
     </>
